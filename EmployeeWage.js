905a6630ed6eaddea02d513fb7f757e688817ede
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-07-05 17:29:56 +0530

    [Madhavi] wage for hours and days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,6 +5,7 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const EMP_WAGE_PERHOUR = 20;
 const EMP_MAX_WORKINGDAYS = 20;
+const EMP_MAX_WORKINGHOURS = 160;
 
 function getEmpWorkingHours(empCheck)
 {  
@@ -26,15 +27,16 @@ function getEmpWorkingHours(empCheck)
 }
 
 {
-    let empSalaryForAMonth = 0,employeeWageForADay,empHours;
-
-    for(let i = 0; i < EMP_MAX_WORKINGDAYS; i++)
+    let empSalaryForAMonth = 0,employeeWageForADay = 0,empHours = 0,totalEmpHours = 0,totalWorkingDays = 0;
+    while(totalEmpHours < EMP_MAX_WORKINGHOURS && totalWorkingDays < EMP_MAX_WORKINGDAYS)
     {
         empCheck = Math.floor(Math.random() * 10) % 3;
         empHours = getEmpWorkingHours(empCheck);
+        totalEmpHours = totalEmpHours + empHours;
         employeeWageForADay = empHours * EMP_WAGE_PERHOUR;
         console.log("Employee Wage for a Day: " + employeeWageForADay);
         empSalaryForAMonth = empSalaryForAMonth + employeeWageForADay;
+        totalWorkingDays++;
     }
-    console.log("\nEmployee Salary for a Month: " + empSalaryForAMonth);
+    console.log("\nTotal Working Hours: " + totalEmpHours + "\nTotal Working Days: " + totalWorkingDays + "\nEmployee Salary for a Month: " + empSalaryForAMonth);
 }
