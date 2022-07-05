2975c6e4d48dc3a7e573b64d5e38ea42accfa42a
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-07-05 17:18:54 +0530

    [Madhavi] calculate employee wage using function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,33 +1,34 @@
-{
-    const IS_ABSENT = 0;
-    const IS_PART_TIME = 1;
-    const IS_FULL_TIME = 2;
-    const PART_TIME_HOURS = 4;
-    const FULL_TIME_HOURS = 8;
-    const EMP_WAGE_PERHOUR = 20;
-    let employeeWageForADay;
+const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8; 
+const EMP_WAGE_PERHOUR = 20;
 
-    let random = Math.floor(Math.random() * 10) % 3;
-    switch(random)
+function getEmpWorkingHours(empCheck)
+{ 
+    switch(empCheck)
     {
         case IS_ABSENT:
         {
             console.log("Employee is Absent");
-            break;
+            return IS_ABSENT;
         }
         case IS_PART_TIME:
         {
             console.log("Employee is Working Part Time");
-            employeeWageForADay = PART_TIME_HOURS * EMP_WAGE_PERHOUR;
-            console.log("Employee Wage For a Day: " + employeeWageForADay);
-            break;
+            return PART_TIME_HOURS;
         }
         case IS_FULL_TIME:
         {
             console.log("Employee is Working Full Time");
-            employeeWageForADay = FULL_TIME_HOURS * EMP_WAGE_PERHOUR;
-            console.log("Employee Wage For a Day: " + employeeWageForADay);
-            break;
+            return FULL_TIME_HOURS;
         }
     }
+}
+{
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHours = getEmpWorkingHours(empCheck);
+    let employeeWageForADay = empHours * EMP_WAGE_PERHOUR;
+    console.log("Employee Wage for a Day: " + employeeWageForADay);
 }
