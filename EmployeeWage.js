97ac7939da3fafb90d07a47a66f402c17e438d38
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-07-05 17:08:46 +0530

    [Madhavi] part time or full time

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,33 @@
 {
     const IS_ABSENT = 0;
-    let random = Math.floor(Math.random() * 10) % 2;
-    if(random == IS_ABSENT)
+    const IS_PART_TIME = 1;
+    const IS_FULL_TIME = 2;
+    const PART_TIME_HOURS = 4;
+    const FULL_TIME_HOURS = 8;
+    const EMP_WAGE_PERHOUR = 20;
+    let employeeWageForADay;
+
+    let random = Math.floor(Math.random() * 10) % 3;
+    switch(random)
     {
-        console.log("Employee is Absent");
-        return;
+        case IS_ABSENT:
+        {
+            console.log("Employee is Absent");
+            break;
+        }
+        case IS_PART_TIME:
+        {
+            console.log("Employee is Working Part Time");
+            employeeWageForADay = PART_TIME_HOURS * EMP_WAGE_PERHOUR;
+            console.log("Employee Wage For a Day: " + employeeWageForADay);
+            break;
+        }
+        case IS_FULL_TIME:
+        {
+            console.log("Employee is Working Full Time");
+            employeeWageForADay = FULL_TIME_HOURS * EMP_WAGE_PERHOUR;
+            console.log("Employee Wage For a Day: " + employeeWageForADay);
+            break;
+        }
     }
-    console.log("Employee is Present");
 }
