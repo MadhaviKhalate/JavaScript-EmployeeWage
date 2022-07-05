bfe1d8b305a6993890a57c1b2c3e5e6739523895
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-07-05 17:38:48 +0530

    [Madhavi] array helper function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -42,8 +42,64 @@ function calculateWage(empHours)
         totalEmpHours = totalEmpHours + empHours;
         totalWorkingDays++;
     }
+    
+    //ForEach Method
     dailyWageArray.forEach(element => {
         empSalaryForAMonth = empSalaryForAMonth + element;
     });
-    console.log("\nTotal Working Hours: " + totalEmpHours + "\nTotal Working Days: " + totalWorkingDays + "\nEmployee Salary for a Month: " + empSalaryForAMonth);
+    console.log("Total Working Hours: " + totalEmpHours + "\nTotal Working Days: " + totalWorkingDays + "\nEmployee Salary for a Month: " + empSalaryForAMonth);
+    
+    //Reduce Method
+    function reduceFunction(employeeWageForADay,totalSalary)
+    {
+        return totalSalary = totalSalary + employeeWageForADay;
+    }
+    let monthlyWageReduceMethod = dailyWageArray.reduce(reduceFunction, 0);
+    console.log("Total Working Hours : " + totalEmpHours + "\nTotal Working Days : " + totalWorkingDays + "\nEmployee Salary for a Month : " + monthlyWageReduceMethod);
+    
+    //Mapping Day and its Wage
+    let dayCounter = 0;
+    function mapForDayAndWage(employeeWageForADay)
+    {
+        dayCounter++;
+        return dayCounter + " = " + employeeWageForADay;
+    }
+    let mapDaywithWageArray = dailyWageArray.map(mapForDayAndWage);
+    console.log(mapDaywithWageArray);
+    
+    //Getting number of times full time wage is Earned
+    function fullTimeWage(employeeWageForADay)
+    {
+        return employeeWageForADay.includes("160");
+    }
+    let fullTimeWageArray = mapDaywithWageArray.filter(fullTimeWage);
+    console.log(fullTimeWageArray);
+    
+    //Getting first occurence of full time wage is Earned
+    let firstFullTimeArray = mapDaywithWageArray.find(fullTimeWage);
+    console.log(firstFullTimeArray);
+    
+    //Checking is Full time wage Array has only containing 160
+    let fullTimeWageCheck = fullTimeWageArray.every(fullTimeWage);
+    console.log("Is Full Time Wage array only contains 160: " + fullTimeWageCheck);
+    
+    //Checking is there any Part Time Wage in Employee Wage Array
+    function partTimeWage (employeeWageForADay)
+    {
+        return employeeWageForADay.includes("80");
+    };
+    let partTimeWageCheck = mapDaywithWageArray.some(partTimeWage);
+    console.log("Is Employee wage contains Part time wage of 80: " + partTimeWageCheck);
+    
+    //Finding number of Days Employee is Worked
+    function employeePresentDays(presentDays, employeeWageForADay)
+    {
+        if(employeeWageForADay > 0)
+        {
+            return presentDays+1;
+        }
+        return presentDays;
+    }
+    let employeePresentDaysArray = dailyWageArray.reduce(employeePresentDays, 0);
+    console.log(employeePresentDaysArray);
 }
