2ed7fc441d4703f06770b9814cee4379533a6c12
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-07-06 15:00:21 +0530

    [Madhavi] arrow function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -34,6 +34,7 @@ function calculateWage(empHours)
     let empSalaryForAMonth = 0,employeeWageForADay = 0,empHours = 0,totalEmpHours = 0,totalWorkingDays = 0;
     let dailyWageArray = new Array();
     let empWageMap = new Map();
+    let empHrsMap = new Map();
     while(totalEmpHours < EMP_MAX_WORKINGHOURS && totalWorkingDays < EMP_MAX_WORKINGDAYS)
     {
         empCheck = Math.floor(Math.random() * 10) % 3;
@@ -41,17 +42,18 @@ function calculateWage(empHours)
         employeeWageForADay = calculateWage(empHours);
         dailyWageArray.push(employeeWageForADay);
         empWageMap.set(totalWorkingDays,employeeWageForADay);
+        empHrsMap.set(totalWorkingDays, empHours);
         totalEmpHours = totalEmpHours + empHours;
         totalWorkingDays++;
     }
     
-    //ForEach Method
+    //UC7A-ForEach Method
     dailyWageArray.forEach(element => {
         empSalaryForAMonth = empSalaryForAMonth + element;
     });
     console.log("Total Working Hours: " + totalEmpHours + "\nTotal Working Days: " + totalWorkingDays + "\nEmployee Salary for a Month: " + empSalaryForAMonth);
     
-    //Reduce Method
+    //UC7A-Reduce Method
     function totalWages(employeeWageForADay,totalSalary)
     {
         return totalSalary = totalSalary + employeeWageForADay;
@@ -59,7 +61,7 @@ function calculateWage(empHours)
     let monthlyWageReduceMethod = dailyWageArray.reduce(totalWages, 0);
     console.log("Total Working Hours : " + totalEmpHours + "\nTotal Working Days : " + totalWorkingDays + "\nEmployee Salary for a Month : " + monthlyWageReduceMethod);
     
-    //Mapping Day and its Wage
+    //UC7B-Mapping Day and its Wage
     let dayCounter = 0;
     function mapForDayAndWage(employeeWageForADay)
     {
@@ -69,7 +71,7 @@ function calculateWage(empHours)
     let mapDaywithWageArray = dailyWageArray.map(mapForDayAndWage);
     console.log(mapDaywithWageArray);
     
-    //Getting number of times full time wage is Earned
+    //UC7C-Getting number of times full time wage is Earned
     function fullTimeWage(employeeWageForADay)
     {
         return employeeWageForADay.includes("160");
@@ -77,15 +79,15 @@ function calculateWage(empHours)
     let fullTimeWageArray = mapDaywithWageArray.filter(fullTimeWage);
     console.log(fullTimeWageArray);
     
-    //Getting first occurence of full time wage is Earned
+    //UC7D-Getting first occurence of full time wage is Earned
     let firstFullTimeArray = mapDaywithWageArray.find(fullTimeWage);
     console.log(firstFullTimeArray);
     
-    //Checking is Full time wage Array has only containing 160
+    //UC7E-Checking is Full time wage Array has only containing 160
     let fullTimeWageCheck = fullTimeWageArray.every(fullTimeWage);
     console.log("Is Full Time Wage array only contains 160: " + fullTimeWageCheck);
     
-    //Checking is there any Part Time Wage in Employee Wage Array
+    //UC7F-Checking is there any Part Time Wage in Employee Wage Array
     function partTimeWage (employeeWageForADay)
     {
         return employeeWageForADay.includes("80");
@@ -93,7 +95,7 @@ function calculateWage(empHours)
     let partTimeWageCheck = mapDaywithWageArray.some(partTimeWage);
     console.log("Is Employee wage contains Part time wage of 80: " + partTimeWageCheck);
     
-    //Finding number of Days Employee is Worked
+    //UC7G-Finding number of Days Employee is Worked
     function employeePresentDays(presentDays, employeeWageForADay)
     {
         if(employeeWageForADay > 0)
@@ -105,7 +107,7 @@ function calculateWage(empHours)
     let employeePresentDaysArray = dailyWageArray.reduce(employeePresentDays, 0);
     console.log("Number of Days Employee present in a Month: " + employeePresentDaysArray);
 
-    //Storing Day and DailyWage along with TotalWage
+    //UC8-Storing Day and DailyWage along with TotalWage
     dayCounter = 0;
     empSalaryForAMonth = 0;
     function dailyAndTotalWage(employeeWageForADay)
@@ -117,4 +119,38 @@ function calculateWage(empHours)
     let dailyAndTotalWageArray = dailyWageArray.map(dailyAndTotalWage);
     console.log(dailyAndTotalWageArray);
     console.log("Total Wage using Map Function: " + Array.from(empWageMap.values()).reduce(totalWages, 0));
+
+    //UC9-Arrow Function
+    const findTotal = (totalVal, dailyVal) =>{
+        return totalVal + dailyVal;
+    }
+    let totalHoursArrow = Array.from(empHrsMap.values()).reduce(findTotal, 0);
+    console.log("Total Hours Worked by an Employee: " + totalHoursArrow);
+    let totalWageArrow = dailyWageArray.filter(employeeWageForADay => employeeWageForADay > 0).reduce(findTotal, 0);
+    console.log("Total Salary earned by an Employee: " + totalWageArrow);
+
+    let fullWorkingDays = new Array();
+    let PartWorkingDays = new Array();
+    let noWorkingDays = new Array();
+
+    empHrsMap.forEach((value, key, map) =>{
+        if(value == 8)
+        {
+            fullWorkingDays.push(key);
+        }
+        else if(value == 4)
+        {
+            PartWorkingDays.push(key);
+        }
+        else
+        {
+            noWorkingDays.push(key);
+        }
+    });
+    console.log("Days Employee worked Full Time:");
+    console.log(fullWorkingDays);
+    console.log("Days Employee worked Part Time: ");
+    console.log(PartWorkingDays);
+    console.log("Days Employee is Absent: ");
+    console.log(noWorkingDays);
 }
