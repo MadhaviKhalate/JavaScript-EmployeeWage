1ebf95cedc17005654d61403c3faabecb5a4a4e1
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-07-06 17:09:29 +0530

    [Madhavi] arrow function in object

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -53,5 +53,33 @@ function calculateWage(empHours)
         totalWorkingDays++;
     }
     console.log("Employee Hours Worked and Wages Earned: " + empDailyHrsAndWageArray);
-    console.log("Employee Wage for a Month: " + empSalaryForAMonth);
+    //UC11A - Calculating Total Hours and Total Wage
+    let totalWage = empDailyHrsAndWageArray.filter(x => x.dailyWage > 0).reduce((wage, x) => wage += x.dailyWage, 0);
+    let totalHours  = empDailyHrsAndWageArray.filter(x => x.dailyHours > 0).reduce((hour, x) => hour += x.dailyHours, 0);
+    console.log("Employee Worked: " + totalHours + " Hours and Earned: " + totalWage);
+
+    //UC11B- Checking Number of Full Working Hours
+    let fullTimeArray = new Array();
+    //empDailyHrsAndWageArray.filter(x => x.dailyHours == 8).forEach(x => console.log(x.toString()));
+    empDailyHrsAndWageArray.filter(x => x.dailyHours == 8).forEach(x => fullTimeArray.push(x));
+    console.log("\nEmployee's all Full Time Woking Days: " + fullTimeArray.length);
+    console.log(fullTimeArray.toString());
+
+    //UC11C-Checking Number of Part Time Working Hours
+    console.log("\nEmployee's all Part Time Woking Days");
+    let partTimeHours = empDailyHrsAndWageArray.filter(x => x.dailyHours == 4).map(x => x.toString());
+    console.log(partTimeHours);
+
+    //UC11D-Checking Number of Non Working Hours
+    console.log("\nEmployee's all Non Woking Days");
+    let noWorkingHours =  empDailyHrsAndWageArray.filter(x => x.dailyHours == 0).map(x => x.toString());
+    console.log(noWorkingHours);
+
+    //UC11E-Getting First occurence of Full Time Wage
+    let firstFullTimeWage = empDailyHrsAndWageArray.find(x => x.dailyWage == 160)
+    console.log("\nEmployee's First Full time Wage Occured at: " + firstFullTimeWage.toString());
+
+    //UC11F-Checking no.of Days Employee is Worked
+    let workingDays = empDailyHrsAndWageArray.filter(x => x.dailyHours > 0);
+    console.log("\nNumber of Days Employee Worked in a Month: " + workingDays.length);
 }
