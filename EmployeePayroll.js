c038c3d51bd4b5356c1c4a9fc2f28325833c8b07
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-07-06 17:32:51 +0530

    [Madhavi] gender and start date

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -2,12 +2,16 @@ class EmployeePayroll
 {
     id; 
     salary;
+    gender;
+    startDate;
 
-    constructor(id, name, salary)
+    constructor(...params)
     {
-        this.id = id;
-        this.name = name
-        this.salary = salary;
+        this.id = params[0];
+        this.name = params[1]
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     get name() 
@@ -21,12 +25,18 @@ class EmployeePayroll
 
     Employee()
     {
-        return "Id: " + this.id + "\t\tName: " + this.name + "\t\tSalary: " + this.salary;
+        
+        return "Id: " + this.id + ",Name: " + this.name + ",Salary: " + this.salary + ",Gender: " + this.gender + ",StartDate: " + this.startDate;
     }
 }
 {
-    let employeePayroll = new EmployeePayroll(1, "Madhavi", 40000);
+    let employeePayroll = new EmployeePayroll(1, "Madhavi", 30000);
     console.log(employeePayroll.Employee());
-    employeePayroll.name = "Khalate";
+    employeePayroll.id = 2;
+    employeePayroll.name = "Poonam";
+    employeePayroll.salary = 30000;
     console.log(employeePayroll.Employee());
+    const date = new Date();
+    let EmployeeData = new EmployeePayroll(3, "Sunil", 25000, "M", date.toLocaleDateString());
+    console.log(EmployeeData.Employee());
 }
